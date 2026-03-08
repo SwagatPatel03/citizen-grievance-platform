e6d4c640f7c1876e6ed4c0a40201c7b7d4e4b9ab
SwagatPatel <swagatpatel03@gmail.com>
2026-03-08 20:34:12 +0530

    add department repository

--- backend/src/main/java/org/example/backend/repository/DepartmentRepository.java
+++ backend/src/main/java/org/example/backend/repository/DepartmentRepository.java
@@ -1,4 +1,8 @@
 package org.example.backend.repository;
 
-public interface DepartmentRepository {
+import org.example.backend.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+    boolean existsByName(String name);
 }
