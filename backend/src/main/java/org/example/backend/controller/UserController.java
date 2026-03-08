d2448fe08e2c18dce76a7af8292893f8a51f4fb1
SwagatPatel <swagatpatel03@gmail.com>
2026-03-08 12:24:38 +0530

    Controller Layer: create endpoint to listen for post request to create a new user

---
+++ backend/src/main/java/org/example/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package org.example.backend.controller;
+
+import org.example.backend.entity.User;
+import org.example.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        try {
+            User savedUser = userService.registerUser(user);
+            return ResponseEntity.ok(savedUser);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+}
