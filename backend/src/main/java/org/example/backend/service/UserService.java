0c799d0e374d077968b952a1f651ef94f2ea6c53
SwagatPatel <swagatpatel03@gmail.com>
2026-03-08 12:12:32 +0530

    Service Layer: checks if an email already exists

---
+++ backend/src/main/java/org/example/backend/service/UserService.java
@@ -0,0 +1,24 @@
+package org.example.backend.service;
+
+import org.example.backend.entity.User;
+import org.example.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        // Check if email is already taken
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new IllegalArgumentException("Email already exists!");
+        }
+        return userRepository.save(user); // Saves the user to Database
+    }
+}
