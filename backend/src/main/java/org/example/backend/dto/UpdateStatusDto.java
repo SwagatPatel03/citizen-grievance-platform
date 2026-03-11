0c1099220ebbdadae1fe6be0e0a1129733293714
SwagatPatel <swagatpatel03@gmail.com>
2026-03-11 16:31:48 +0530

    add update status dto to update complaint's status

---
+++ backend/src/main/java/org/example/backend/dto/UpdateStatusDto.java
@@ -0,0 +1,13 @@
+package org.example.backend.dto;
+
+public class UpdateStatusDto {
+    private String status;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
