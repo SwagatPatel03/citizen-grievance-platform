461349ada01f78908b70d4d9f45e3f4d1894137c
SwagatPatel <swagatpatel03@gmail.com>
2026-03-17 02:08:37 +0530

    set up App.jsx as a basic routing wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Route, Router, Routes} from "react-router-dom";
+
+const Home = () => <h1>LokShikayat Portal</h1>
+const CitizenDashboard = () => <h1>Citizen Dashboard</h1>
+const OfficerDashboard = () => <h1>Officer Dashboard</h1>
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return(
+        <Router>
+            <div className="app-container">
+                {/* We will add a persistent Navbar component here later */}
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/citizen" element={<CitizenDashboard />} />
+                    <Route path="/officer" element={<OfficerDashboard />} />
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
-export default App
+export default App;
