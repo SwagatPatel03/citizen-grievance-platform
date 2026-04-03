8b22d560e7b4a731cc80c826aed55a3688cee065
SwagatPatel <swagatpatel03@gmail.com>
2026-04-03 18:36:26 +0530

    add findByEmail method

--- backend/src/main/java/org/example/backend/repository/UserRepository.java
+++ backend/src/main/java/org/example/backend/repository/UserRepository.java
@@ -4,9 +4,12 @@ import org.example.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     // Spring Data JPA automatically provides methods like save(), findById(), findAll().
     // You can add custom queries here later, like finding a user by email:
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
