b418c34840d947f99e545b8825320fb270a5614e
SwagatPatel <swagatpatel03@gmail.com>
2026-04-03 18:37:34 +0530

    create JwtUtil class to generate token

--- backend/src/main/java/org/example/backend/security/JwtUtil.java
+++ backend/src/main/java/org/example/backend/security/JwtUtil.java
@@ -1,4 +1,65 @@
 package org.example.backend.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
 public class JwtUtil {
+    private static final String SECRET = "LokShikayatSuperSecretKeyForDevelopmentMustBeLongEnough";
+    private final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    private final long EXPIRATION_TIME = 1000 * 60 * 60 * 10;
+
+    public String generateToken(String email, String role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role); // Stores the user's role inside the token
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(email)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String extractRole(String token) {
+        Claims claims = extractAllClaims(token);
+        return (String) claims.get("role");
+    }
+
+    public boolean validateToken(String token, String userEmail) {
+        final String email = extractEmail(token);
+        return (email.equals(userEmail) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token, Claims::getExpiration).before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
 }
